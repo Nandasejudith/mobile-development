d32ae30395a70b81939af81304bd9ebb50522855
Nandasejudith <judithnandase@gmail.com>
2021-02-21 11:03:02 +0300

    assignment1

---
+++ src/mobile development assignment.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val name="nandaseJudith"
+    println(name)
+    var age=20
+    println(age)
+    var weight=64.5
+    println(weight)
+    var phoneNumber=(755033482)
+    println(phoneNumber)
+    var citizenShip=("false")
+    println(citizenShip)
+    var student=arrayOf("nandaseJudith",20,65.4,755033482,"false")
+    println(student)
+
+
+
+}
